03d279192d56e046e5b262b87c69d208962149e6
Kees <kees.bl@gmail.com>
2021-10-25 12:12:48 -0700

    Added progress outputs and made script non-locking
    I added a check that looks for previously exported depth maps. If depth maps were already exported in the selected output folder, they are not re-exported. I've also added print statements that display progress and what was done for each image (skipped because it's not aligned, skipped because it's already exported, or depth maps exported). The script was also made non-locking, so that these statements appear and Metashape doesn't appear to freeze during processing which can take quite a bit of time.

--- exportDepth_v4.py
+++ exportDepth_v4.py
@@ -1,29 +1,64 @@
 # This script exports a SCALED DENSE depth map, after MESH stage has been completed, and a scale has been entered.
 # If no scale is entered, depth maps have relative scale.
-# Exported depth maps are the same size as the original images in the chunk, but compressed. This level of compression does not affect the accuracy needed for color correciton.
+# Exported depth maps are the same size as the original images in the chunk, but compressed. 
+# This level of compression does not affect the accuracy needed for color correciton.
 # Derya Akkaynak
 
 import Metashape
+import os
+from os.path import exists
 
-# Select active chunk
-chunk = Metashape.app.document.chunk 
+# General setup
+app = Metashape.app
+chunk = app.document.chunk
+
+# Set output folder
+outputFolder = app.getExistingDirectory("Select Output Folder")
 
 # Is the model scaled? If not assign scale of 1
 if chunk.transform.scale is None:
     scale = 1
 else:
 	scale = chunk.transform.scale
-		
-# Export depth maps for all aligned cameras in chunk		
-for camera in chunk.cameras:
-	# First check if the camera is aligned
-	if camera.transform:
-		depth = chunk.model.renderDepth(camera.transform, camera.sensor.calibration)
-		depth = depth * scale
-		depth = depth.convert(" ","F16")
-		compr = Metashape.ImageCompression()
-		compr.tiff_compression = Metashape.ImageCompression().TiffCompressionDeflate
-		# Remember to update this path according to your folders
-		depth.save("/Users/deryaakkaynak/Desktop/depth" + camera.label + ".tif", compression = compr)
 
-						
+# For tracking progress
+totalCameras = len(chunk.cameras)
+completedCameras = 0
+
+# Export depth maps for all aligned cameras in chunk 
+for camera in chunk.cameras:
+
+    # Progress tracking
+    completedCameras += 1
+    infoHeading = "[" + str(completedCameras) + "/" + str(totalCameras) + "] [" + camera.label + "] "
+
+    # Don't lock the UI
+    app.update()
+
+	# [Guard] If the image is already aligned, skip
+    imageIsAligned = camera.transform
+    if not imageIsAligned: 
+        print (infoHeading + "Camera is not aligned. Skipping...")
+        continue
+        
+    # [Guard] If the depth map exists in the output folder, skip
+    outputPath = os.path.join(outputFolder, camera.label + "_depth" + ".tif")
+    depthMapAlreadyExists = exists(outputPath)
+    if depthMapAlreadyExists:
+        print (infoHeading + "Depth map has already been exported. Skipping...")
+        continue
+    
+    # Generate depth map
+    # Determine depth and compression
+    depth = chunk.model.renderDepth(camera.transform, camera.sensor.calibration)
+    depth = depth * scale
+    depth = depth.convert(" ","F16")
+    compression = Metashape.ImageCompression()
+    compression.tiff_compression = Metashape.ImageCompression().TiffCompressionDeflate
+    
+    # Export depth map
+    # Save depth to specified folder
+    depth.save(outputPath, compression = compression)
+
+    # Note completion
+    print(infoHeading + "Depth map exported.")
